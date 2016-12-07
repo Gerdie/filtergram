1608a08de7527d010e7a7dcdac5e7b2d8098a441
Gerdie <bdme108@gmail.com>
2016-12-06 22:34:06 -0800

    Experimenting with skimage API

--- filter.py
+++ filter.py
@@ -1,1 +1,13 @@
-from skimage import data, io, filters
+from skimage import data, io, filters, segmentation, color
+import matplotlib.pyplot as plt
+
+#using coffee pic as test example
+coffee = skimage.data.coffee()
+
+#detecting edges with gabor filter
+filt_real, filt_imag = skimage.filters.gabor(coffee, frequency=0.6)
+
+plt.figure()
+
+io.imshow(filt_real)
+io.show()
